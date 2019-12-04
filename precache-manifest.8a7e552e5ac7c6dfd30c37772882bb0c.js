cae7f59a4f25456ff06c61e3fae0f095c3f6fa99
Aadil Khan <aadil.khan@nagarro.com>
2019-12-04 18:25:17 +0530

    Updates

---
+++ precache-manifest.8a7e552e5ac7c6dfd30c37772882bb0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92adac6d5c069dc47cc24154257e895d",
+    "url": "/atm/index.html"
+  },
+  {
+    "revision": "9b2afc55a89898368a10",
+    "url": "/atm/static/css/main.aafcba21.chunk.css"
+  },
+  {
+    "revision": "85aae177f134652c93a0",
+    "url": "/atm/static/js/2.a9ef9e7e.chunk.js"
+  },
+  {
+    "revision": "9b2afc55a89898368a10",
+    "url": "/atm/static/js/main.a39f053a.chunk.js"
+  },
+  {
+    "revision": "512fbcaee48f6ecd3aea",
+    "url": "/atm/static/js/runtime-main.d0cabd2c.js"
+  }
+]);
